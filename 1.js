d6f99dd9b511026d14904f6a56fc9d84972ecb3e
fcu-d0342435 <d0342435@fcu.edu.tw>
2018-01-11 12:14:48 +0800

    使用node.js做mysql和express的佈局，整合bootstrap的網頁轉成ejs的樣式

--- 1.js
+++
@@ -1,9 +0,0 @@
-$("#SendComment").click(function(){
-	var text1 = $("#Comment").val();
-	console.log(text1);
-	$("#comment_1").append("<h1>"+ text1 +"</h1>");
-});
-function test(){
-	console.log('1');
-//ด๚ธี
-}
---
+++ routes/index.js
@@ -0,0 +1,125 @@
+var express = require('express');
+var router = express.Router();
+
+
+/*
+//test db
+// home page
+router.get('/', function(req, res, next) {
+
+    var db = req.con;
+    var data = "";
+
+    db.query('SELECT * FROM project', function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+        var data = rows;
+
+        // use index.ejs
+        res.render('index', { title: 'Account Information', data: data});
+    });
+
+});
+// add page
+router.get('/add', function(req, res, next) {
+
+    // use ProjectAdd.ejs
+    res.render('projectAdd', { title: 'Add Project'});
+});
+
+// add post
+router.post('/projectAdd', function(req, res, next) {
+
+    var db = req.con;
+
+    var sql = {
+        Pname: req.body.Pname,
+        Pnum: req.body.Pnum,
+        Super_SSn: req.body.Super_SSn
+    };
+
+    console.log(sql);
+    var qur = db.query('INSERT INTO project SET ?', sql, function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/');
+    });
+
+});
+// edit page
+router.get('/projectEdit', function(req, res, next) {
+
+    var Pnum = req.query.Pnum;
+    var db = req.con;
+    var data = "";
+
+    db.query('SELECT * FROM project WHERE Pnum = ?', Pnum, function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+
+        var data = rows;
+        res.render('projectEdit', { title: 'Edit Project', data: data });
+    });
+//edit update
+router.post('/projectEdit', function(req, res, next) {
+
+    var db = req.con;
+    var Pnum = req.body.Pnum;
+
+    var sql = {
+        //Pname: req.body.Pname,
+        Pnum: req.body.Pnum,
+        //Super_SSn: req.body.Super_SSn
+    };
+
+    var qur = db.query('UPDATE project SET ? WHERE Pnum = ?', [sql, Pnum], function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/');
+    });
+
+});
+
+});
+//test db end
+*/
+//boostrap
+// home page
+router.get('/', function(req, res, next) {
+
+        // use index.ejs
+        res.render('index', { title: 'index Information'});
+});
+router.get('/about', function(req, res, next) {
+
+    // use ProjectAdd.ejs
+    res.render('about', { title: 'about Tea'});
+});
+
+router.get('/forum', function(req, res, next) {
+
+    // use ProjectAdd.ejs
+    res.render('forum', { title: 'forum imformation'});
+});
+
+router.get('/intro', function(req, res, next) {
+
+    // use ProjectAdd.ejs
+    res.render('intro', { title: 'intro Tea'});
+});
+
+router.get('/store', function(req, res, next) {
+
+    // use ProjectAdd.ejs
+    res.render('store', { title: 'store imformation'});
+});
+
+
+module.exports = router;
